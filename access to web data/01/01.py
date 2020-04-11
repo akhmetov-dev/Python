92ab0b64ba3093fd9f7fed749ce0b066d6a13359
akhmetov-dev <42318239+akhmetov-dev@users.noreply.github.com>
2020-04-11 18:31:06 +0300

    01

--- access to web data/01/01.py
+++ access to web data/01/01.py
@@ -3,7 +3,7 @@ import re
 name = input("Enter name of the file:")
 
 if len(name) == 0 :
-    name = "sample.txt"
+    name = "regex_sum_410867.txt"
 
 try :
     handle = open(name)
@@ -11,6 +11,16 @@ except :
     print("Something wrong.")
     quit()
 
+tmp = list()
+
+sum = 0
+count = 0
+
 for line in handle :
-    tmp = re.findall("^V.*:\s([0-9]+)", line)
-    print(tmp)
+    tmp = re.findall("([0-9]+)", line)
+    for i in tmp :
+        sum = sum + int(i)
+        count = count + 1
+        print(i)
+
+print(count, sum)
